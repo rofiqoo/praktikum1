712ce9f194e288747a9f6765da844b9957cd2ece
rofiqoo <146154406+rofiqoo@users.noreply.github.com>
2023-09-27 10:28:01 +0700

    Add files via upload

---
+++ praktikum ap1/minggu 2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+  int tugas, uts, uas;
+  tugas = 80;
+  uts = 85;
+  uas = 90;
+
+  float rata_rata;
+  rata_rata = (tugas + uts + uas) / 3;
+
+  cout << rata_rata << endl;
+}
